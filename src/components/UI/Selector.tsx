08c0dacd01a5da29dd779aa2b29f806c03f56e6c
sobhan <kiko2723@gmial.com>
2024-07-28 22:18:33 +0330

    7/28/2024

---
+++ src/components/UI/Selector.tsx
@@ -0,0 +1,46 @@
+import { textBody2, textBody1Bold } from "../../constants/styles";
+import { cn } from "../../utils/lib/cn";
+import AppsSort from "../UI/icons/AppsSort";
+import LeftArrow from "./icons/LeftArrow";
+
+const Selector = () => {
+
+    return (
+        <main className="flex gap-5 my-2">
+            <section className="flex flex-col w-[218px] h-[347px] gap-2">
+                <div className={`${cn("", textBody2)} flex justify-center items-center gap-2 bg-main-green-300 rounded-small h-[57px] cursor-pointer`}>
+                    <div className='flex justify-between items-center w-[163px] h-[25px]'>
+                        <div className='flex gap-2'>
+                            <AppsSort className="fill-main-black" />
+                            <p className={`${cn("", textBody1Bold)} text-main-black`}>دسته بندی</p>
+                        </div>
+                        <LeftArrow className="w-4 h-4 -rotate-90" />
+                    </div>
+                </div>
+                <ul className={`flex flex-col h-[282px] rounded-small border-[1.5px] bg-main-gray-500 border-main-gray-400 box-shadow-1 gap-[2px] p-[10px] cursor-pointer z-50 transiton-all duration-200 ${cn("", textBody2)}`}>
+                    <li className="w-[198px] h-[42px] rounded-small gap-4 pt-[10px] pr-[10px] pb-[10px] pl-[103px] text-main-white hover:bg-main-gray-400">
+                        <span className="w-[85px] h-[22px]">زیست شناسی</span>
+                    </li>
+                </ul>
+            </section>
+            <section className="flex flex-col w-[218px] h-[347px] gap-2">
+                <div className={`${cn("", textBody2)} flex justify-center items-center gap-2 rounded-small h-[57px] border-2 border-main-white cursor-pointer`}>
+                    <div className='flex justify-between items-center w-[163px] h-[25px]'>
+                        <div className='flex gap-2'>
+                            <AppsSort className="fill-main-white" />
+                            <p className={`${cn("", textBody1Bold)}`}>مرتب سازی</p>
+                        </div>
+                        <LeftArrow className="w-4 h-4 -rotate-90 fill-main-white" />
+                    </div>
+                </div>
+                <ul className={`flex flex-col h-[140px] rounded-small border-[1.5px] bg-main-gray-500 border-main-gray-400 box-shadow-1 gap-[2px] p-[10px] cursor-pointer z-50 transiton-all duration-200 ${cn("", textBody2)}`}>
+                    <li className="w-[198px] h-[42px] rounded-small gap-4 pt-[10px] pr-[10px] pb-[10px] pl-[103px] text-main-white hover:bg-main-gray-400">
+                        <span className="w-[85px] h-[22px]">گران ترین</span>
+                    </li>
+                </ul>
+            </section>
+        </main>
+    )
+}
+
+export default Selector;
--- src/pages/public/Courses.tsx
+++ src/pages/public/Courses.tsx
@@ -1,18 +1,17 @@
-import CoursesBox from "../../components/CoursesBox";
-import SelectorContent from "../../components/UI/SelectorContent";
-import { textTitle1, bgTextColor, textBody1 } from "../../constants/styles";
+import Selector from "../../components/UI/Selector";
+import { textTitle1, bgTextColor, textBody1Bold } from "../../constants/styles";
 import { cn } from "../../utils/lib/cn";
 
 const Courses = () => {
+
     return (
         <main className="flex flex-col gap-4 justify-center my-6 mx-20">
             <section className="flex flex-col gap-4">
                 <h1 className={` ${cn("", textTitle1, bgTextColor)} px-0`}>دوره های وینسل</h1>
-                <p className={`${cn("", textBody1)} w-11/12 text-justify`}>وینسل یک سایت آموزشی است که به طور خاص بر روی آموزش زیست‌شناسی تمرکز دارد. این سایت منابع متنوعی از جمله مقالات، ویدیوهای آموزشی، آزمون‌های آنلاین و دوره‌های تخصصی را ارائه می‌دهد تا به دانش‌آموزان و دانشجویان کمک کند تا مفاهیم زیست‌شناسی را بهتر درک کنند و در امتحانات خود موفق شوند. وینسل با استفاده از روش‌های نوین آموزشی و بهره‌گیری از اساتید مجرب، محیطی مناسب برای یادگیری فراهم می‌کند.</p>
+                <p className={`${cn("", textBody1Bold)} w-[1244px] h-[78px] text-justify my-2`}>وینسل یک سایت آموزشی است که به طور خاص بر روی آموزش زیست‌شناسی تمرکز دارد. این سایت منابع متنوعی از جمله مقالات، ویدیوهای آموزشی، آزمون‌های آنلاین و دوره‌های تخصصی را ارائه می‌دهد تا به دانش‌آموزان و دانشجویان کمک کند تا مفاهیم زیست‌شناسی را بهتر درک کنند و در امتحانات خود موفق شوند. وینسل با استفاده از روش‌های نوین آموزشی و بهره‌گیری از اساتید مجرب، محیطی مناسب برای یادگیری فراهم می‌کند.</p>
             </section>
-            <SelectorContent />
-            <CoursesBox />
-        </main> 
+            <Selector/>
+        </main>
     )
 }
 
