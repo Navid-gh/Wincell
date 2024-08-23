2533eafed7fac7d5e2f30c208f3ad2dfafd5d4d2
Navid Ghasemi <navid.12.gh@gmail.com>
2024-08-24 02:35:11 +0330

    2:40AM 24 August

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,110 +1,103 @@
-import { GetArticles, GetCourses } from "../types/apiTypes";
+import { GetArticles, GetCourses } from '../types/apiTypes';
 
 export const Endpoints = {
-  // User endpoints
-  registerOtp: `/auth/registerStepOne`,
-  register: `/auth/registerStepTwo`,
-  logIn: `/auth/login`,
-  checkOtp: `/auth/checkOtp`,
-  resetCode: `/auth/resetCode`,
-  refreshToken: `/auth/refreshToken`,
-  getMyCourses: `/user/getBought`,
-  getMyLikes: `/getAllCourse`,
-  getMyComments: `/user/getAllComment`,
-  getMyCertificates: `/user/getAllCertificate`,
-  getUsers: `/user/getAllUser`,
+    // User endpoints
+    registerOtp: `/auth/registerStepOne`,
+    register: `/auth/registerStepTwo`,
+    logIn: `/auth/login`,
+    checkOtp: `/auth/checkOtp`,
+    resetCode: `/auth/resetCode`,
+    refreshToken: `/auth/refreshToken`,
+    getMyCourses: `/user/getBought`,
+    getMyLikes: `/getAllCourse`,
+    getMyComments: `/user/getAllComment`,
+    getMyCertificates: `/user/getAllCertificate`,
+    getUsers: `/user/getAllUser`,
 
-  // Course endpoints
-  getCourses: (
-    categoryId: GetCourses[0],
-    limit: GetCourses[1],
-    filter: GetCourses[2]
-  ) => `/getAllCourse/${categoryId}/${limit}/${filter}`,
-  getCourse: (courseID: string) => `/getOnecourse/${courseID}`,
-  addCourse: `/createCourse`,
-  deleteCourse: (courseID: string) => `/deleteCourse/${courseID}`,
-  editCourse: (courseID: string) => `/updateCourse/${courseID}`,
-  likeCourse: (courseID: string) => `/course/addLike/${courseID}`,
+    // Course endpoints
+    getCourses: (categoryId: GetCourses[0], limit: GetCourses[1], filter: GetCourses[2]) =>
+        `/getAllCourse/${categoryId}/${limit}/${filter}`,
+    getCourse: (courseID: string) => `/getOnecourse/${courseID}`,
+    addCourse: `/createCourse`,
+    deleteCourse: (courseID: string) => `/deleteCourse/${courseID}`,
+    editCourse: (courseID: string) => `/updateCourse/${courseID}`,
+    likeCourse: (courseID: string) => `/course/addLike/${courseID}`,
 
-  // Article endpoints
-  getArticles: (
-    categoryId: GetArticles[0],
-    limit: GetArticles[1],
-    filter: GetArticles[2]
-  ) => `/getAllBlog/${categoryId}/${limit}/${filter}`,
-  getArticle: (articleID: string) => `/getOneBlog/${articleID}`,
-  addArticle: `/createBlog`,
-  deleteArticle: (articleID: string) => `/deleteBlog/${articleID}`,
-  editArticle: (articleID: string) => `/updateBlog/${articleID}`,
-  likeArticle: (articleID: string) => `/blog/addLike/${articleID}`,
+    // Article endpoints
+    getArticles: (categoryId: GetArticles[0], limit: GetArticles[1], filter: GetArticles[2]) =>
+        `/getAllBlog/${categoryId}/${limit}/${filter}`,
+    getArticle: (articleID: string) => `/getOneBlog/${articleID}`,
+    addArticle: `/createBlog`,
+    deleteArticle: (articleID: string) => `/deleteBlog/${articleID}`,
+    editArticle: (articleID: string) => `/updateBlog/${articleID}`,
+    likeArticle: (articleID: string) => `/blog/addLike/${articleID}`,
 
-  // Chapter endpoints
-  getChapters: (courseID: string) => `/api/chapter/list/${courseID}`,
-  addChapter: `/addChapter`,
-  deleteChapter: (ChapterID: string) => `/chapter/delete/${ChapterID}`,
-  editChapter: (ChapterID: string) => `/chapter/update/${ChapterID}`,
+    // Chapter endpoints
+    getChapters: (courseID: string) => `/api/chapter/list/${courseID}`,
+    addChapter: `/addChapter`,
+    deleteChapter: (ChapterID: string) => `/chapter/delete/${ChapterID}`,
+    editChapter: (ChapterID: string) => `/chapter/update/${ChapterID}`,
 
-  // Episode endpoints
-  getEpisodes: (courseID: string) => `/getEpisodesOfChpater/${courseID}`,
-  addEpisode: `/createEpisode`,
-  deleteEpisode: (EpisodeID: string) => `/deleteEpisode/${EpisodeID}`,
+    // Episode endpoints
+    getEpisodes: (courseID: string) => `/getEpisodesOfChpater/${courseID}`,
+    addEpisode: `/createEpisode`,
+    deleteEpisode: (EpisodeID: string) => `/deleteEpisode/${EpisodeID}`,
 
-  // category
-  getCategories: (type: "course" | "blog" | undefined) =>
-    `/getAllCategory/${type}`,
-  addCategory: `/addCategory`,
-  editCategory: (categoryID: string) => `/updateCategory/${categoryID}`,
-  deleteCategory: (categoryID: string) => `/deleteCategory/${categoryID}`,
-  getOneCategory: (categoryID: string) => `/getOneCategory/${categoryID}`,
+    // category
+    getCategories: (type: 'course' | 'blog' | undefined) => `/getAllCategory/${type}`,
+    addCategory: `/addCategory`,
+    editCategory: (categoryID: string) => `/updateCategory/${categoryID}`,
+    deleteCategory: (categoryID: string) => `/deleteCategory/${categoryID}`,
+    getOneCategory: (categoryID: string) => `/getOneCategory/${categoryID}`,
 
-  // certificate endpoints
-  getCertificates: `/evidence/getAll`,
-  addCertificate: `/evidence/addEvidence`,
+    // certificate endpoints
+    getCertificates: `/evidence/getAll`,
+    addCertificate: `/evidence/addEvidence`,
 
-  // Faqs endpoints
-  getFAQs: `/faq/readFaq/undefined/all`,
-  addFAQ: `/faq/create`,
-  deleteFAQ: (FaqID: string) => `/faq/delete/${FaqID}`,
-  editFAQ: (FaqID: string) => `/admin/faq/edit/${FaqID}`,
+    // Faqs endpoints
+    getFAQs: `/faq/readFaq/undefined/all`,
+    addFAQ: `/faq/create`,
+    deleteFAQ: (FaqID: string) => `/faq/delete/${FaqID}`,
+    editFAQ: (FaqID: string) => `/admin/faq/edit/${FaqID}`,
 
-  // Images endpoints
-  addImages: `/addImage`,
-  editImage: (id: string) => `/admin/image/edit/${id}`,
-  deleteImage: (id: string) => `/admin/image/remove/${id}`,
-  getImages: `/admin/image/list`,
+    // Images endpoints
+    addImages: `/addImage`,
+    editImage: (id: string) => `/admin/image/edit/${id}`,
+    deleteImage: (id: string) => `/admin/image/remove/${id}`,
+    getImages: `/admin/image/list`,
 
-  // basket endpoints
-  payment: `/bascket`,
-  orderDetail: (id: string) => `/api/payment/getAuthority/${id}`,
-  updateBasket: `/basket/update`,
+    // basket endpoints
+    payment: `/bascket`,
+    orderDetail: (id: string) => `/getAuthority/${id}`,
+    updateBasket: `/basket/update`,
 
-  // comment endpoints
-  addComment: `/comment/addComment`,
-  setCommentStatus: (commentId: string) => `/comment/changeStatus/${commentId}`,
-  deleteComment: (commentId: string) => `/comment/deleteComment/${commentId}`,
-  getAllComments: `/comment/readByAdmin`,
+    // comment endpoints
+    addComment: `/comment/addComment`,
+    setCommentStatus: (commentId: string) => `/comment/changeStatus/${commentId}`,
+    deleteComment: (commentId: string) => `/comment/deleteComment/${commentId}`,
+    getAllComments: `/comment/readByAdmin`,
 
-  // filter endpoints
-  searchAll: (query: string) => `/searchAll?search?${query}`,
-  searchCourse: (query: string) => `/searchCourse?search?${query}`,
-  searchBlog: (query: string) => `/searchBlog?search?${query}`,
+    // filter endpoints
+    searchAll: (query: string) => `/searchAll?search?${query}`,
+    searchCourse: (query: string) => `/searchCourse?search?${query}`,
+    searchBlog: (query: string) => `/searchBlog?search?${query}`,
 
-  // sales endpoints
-  getSales: `/basket/getAllSold`,
+    // sales endpoints
+    getSales: `/basket/getAllSold`,
 
-  // discount code endpoints
-  addDiscountCode: `/code/add`,
-  deleteDiscountCode: (discountId: string) => `/code/delete/${discountId}`,
-  checkDiscountCode: `/checkCode`,
-  getCodes: `/code/getAllCode`,
+    // discount code endpoints
+    addDiscountCode: `/code/add`,
+    deleteDiscountCode: (discountId: string) => `/code/delete/${discountId}`,
+    checkDiscountCode: `/checkCode`,
+    getCodes: `/code/getAllCode`,
 
-  // contact US endpoints
-  addTicket: `/tiket/add`,
-  getAllTickets: `/tiket/readAll`,
-  deleteTicket: (ticketId: string) => `/tiket/remove/${ticketId}`,
+    // contact US endpoints
+    addTicket: `/tiket/add`,
+    getAllTickets: `/tiket/readAll`,
+    deleteTicket: (ticketId: string) => `/tiket/remove/${ticketId}`,
 
-  // slideshow slides endpoints
-  getAllSlides: `/slider/getAll`,
-  addSlide: `/slider/addSlider`,
-  removeSlide: (slideId: string) => `/slider/remove/${slideId}`,
+    // slideshow slides endpoints
+    getAllSlides: `/slider/getAll`,
+    addSlide: `/slider/addSlider`,
+    removeSlide: (slideId: string) => `/slider/remove/${slideId}`,
 };
--- src/pages/user/Basket.tsx
+++ src/pages/user/Basket.tsx
@@ -1,173 +1,150 @@
-import { useState } from "react";
-import {
-  textBody1Bold,
-  textBody3,
-  textTitle3
-} from "../../constants/styles";
-import { cn } from "../../utils/lib/cn";
-import BasketIcon from "../../components/UI/icons/Basket";
-import WithLoaderAndError from "../../components/WithLoaderAndError";
-import { useQuery } from "@tanstack/react-query";
-import { useAuth, useAuthHooks } from "../../hooks/useAuth";
-import { Payment, updateBasket } from "../../api/basket";
-import { useAppSelector } from "../../hooks/useReduxHooks";
-import { Course } from "../../types/apiTypes";
-import Button from "../../components/UI/Button";
-import Input from "../../components/UI/Input";
-import BasketProducts from '../../components/UI/BasketProducts';
-import Tick from "../../components/UI/icons/Tick";
-import { checkCode } from "../../api";
-import toast from "react-hot-toast";
-import { useNavigate, useParams } from "react-router-dom";
-import BasketDetails from "../../components/UI/BasketDetails";
+import { useEffect, useState } from 'react';
+import { textBody1Bold, textBody3, textTitle3 } from '../../constants/styles';
+import { cn } from '../../utils/lib/cn';
+import BasketIcon from '../../components/UI/icons/Basket';
+import WithLoaderAndError from '../../components/WithLoaderAndError';
+import { useQuery } from '@tanstack/react-query';
+import { useAuth, useAuthHooks } from '../../hooks/useAuth';
+import { Payment, updateBasket } from '../../api/basket';
+import { useAppDispatch, useAppSelector } from '../../hooks/useReduxHooks';
+import { Course } from '../../types/apiTypes';
+import Button from '../../components/UI/Button';
+import Input from '../../components/UI/Input';
+import Tick from '../../components/UI/icons/Tick';
+import { checkCode } from '../../api';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import BasketDetails from '../../components/UI/BasketDetails';
+import BasketProducts from '../../components/UI/basketProducts';
+import { updateProduct } from '../../redux/basketSlice';
 
 const Basket = () => {
+    const [products, setProducts] = useState<Course[]>([]);
+    const navigate = useNavigate();
+    const { token } = useAuth();
+    const authHooks = useAuthHooks();
+    const basketData = useAppSelector(state => state.basket);
+    const dispatch = useAppDispatch();
+    const { data, isError, isLoading, error } = useQuery({
+        queryKey: ['basket', basketData.productsId],
+        queryFn: () => updateBasket({ token, ...authHooks }, basketData.productsId),
+    });
 
-  const { id } = useParams();
+    useEffect(() => {
+        setProducts(data || []);
+    }, [data]);
 
-  const navigate = useNavigate();
+    const handleBasket = async (courseId: string) => {
+        const newProducts = products.filter(product => product._id !== courseId);
+        dispatch(
+            updateProduct({
+                productsId: newProducts.map(product => product._id),
+                qty: newProducts.length,
+            })
+        );
+        setProducts(newProducts);
+    };
 
-  const { token } = useAuth();
-  const authHooks = useAuthHooks();
-  const basketData = useAppSelector((state) => state.basket);
-  const { data, isError, isLoading, error } = useQuery({
-    queryKey: ["basket", basketData.productsId],
-    queryFn: () => updateBasket({ token, ...authHooks }, basketData.productsId),
-  });
+    const [discountCode, setDiscountCode] = useState('');
+    const [discountLoading, setDiscountLoading] = useState(false);
+    const [discountResult, setDiscountResult] = useState(0);
 
-  const handleBasket = async (courseId: string) => {
-    try {
-      const updatedCourseIds = basketData.productsId.filter(id => id !== courseId);
-      const response = await updateBasket({ token, ...authHooks }, updatedCourseIds);
-      console.log("Basket updated:", response);
-    } catch (err) {
-      console.error("Error updating basket:", err);
-    }
-  }
+    const totalPrice = products.reduce((total: number, course: Course) => total + course.priceAfterDiscount, 0) || 0;
+    const totalDiscount = products.reduce((total: number, course: Course) => total + (course.discount + discountResult || 0), 0) || 0;
+    const totalPriceWithDiscount =
+        products.reduce(
+            (total: number, course: Course) => total + (course.priceAfterDiscount - (course.discount + discountResult || 0)),
+            0
+        ) || 0;
 
-  const [discountCode, setDiscountCode] = useState("");
-  const [discountLoading, setDiscountLoading] = useState(false);
-  const [discountResult, setDiscountResult] = useState(0);
+    const handleCheckCode = async () => {
+        setDiscountLoading(true);
+        try {
+            const response = await checkCode({ token, ...authHooks }, discountCode);
+            if (response.valid) {
+                setDiscountResult(response.discount);
+                console.log('Discount code is valid: ', response.discount);
+                toast.success('کد تخفیف با موفقیت اعمال شد');
+            } else {
+                setDiscountResult(0);
+                toast.error('کد تخفیف نامعتبر می باشد');
+            }
+        } catch (err) {
+            console.error('Error checking discount code:', err);
+            setDiscountResult(0);
+            toast.error('کد تخفیف نامعتبر می باشد');
+        } finally {
+            setDiscountLoading(false);
+        }
+    };
 
-  const totalPrice =
-    data?.listCourse.reduce(
-      (total: number, course: Course) => total + course.price,
-      0
-    ) || 0;
-  const totalDiscount =
-    data?.listCourse.reduce(
-      (total: number, course: Course) =>
-        total + (course.discount + discountResult || 0),
-      0
-    ) || 0;
-  const totalPriceWithDiscount =
-    data?.listCourse.reduce(
-      (total: number, course: Course) =>
-        total + (course.price - (course.discount + discountResult || 0)),
-      0
-    ) || 0;
+    const handlePurchase = async () => {
+        const basketIds = products.map(product => product._id);
+        try {
+            const paymentResponse = await Payment({ token, ...authHooks }, basketIds);
+            console.log(paymentResponse);
+        } catch (error) {
+            console.error('Error during payment:', error);
+            toast.error('خطا در پردازش پرداخت');
+        }
+    };
 
-  const handleCheckCode = async () => {
-    setDiscountLoading(true);
-    try {
-      const response = await checkCode({ token, ...authHooks }, discountCode);
-      if (response.valid) {
-        setDiscountResult(response.discount);
-        console.log("Discount code is valid: ", response.discount);
-        toast.success("کد تخفیف با موفقیت اعمال شد");
-      } else {
-        setDiscountResult(0);
-        toast.error('کد تخفیف نامعتبر می باشد')
-      }
-    } catch (err) {
-      console.error("Error checking discount code:", err);
-      setDiscountResult(0);
-      toast.error('کد تخفیف نامعتبر می باشد');
-    } finally {
-      setDiscountLoading(false);
-    }
-  };
-
-  const handlePurchase = async () => {
-    const basketIds = basketData.productsId.join(',');
-    try {
-      const paymentResponse = await Payment({ token, ...authHooks }, basketIds);
-      if (paymentResponse.success) {
-        navigate(`/basket/${id}`);
-      } else {
-        toast.error('خطا در پرداخت. لطفا دوباره تلاش کنید.');
-      }
-    } catch (error) {
-      console.error("Error during payment:", error);
-      toast.error('خطا در پردازش پرداخت');
-    }
-  };
-
-  return (
-    <div className="flex flex-col gap-6">
-      <div className="flex items-center rounded-small bg-main-secondary-bg border border-main-primary-text px-6 py-2.5 gap-2.5">
-        <BasketIcon className="w-6 h-6 dark:invert" />
-        <h1 className={textTitle3}>سبد خرید</h1>
-      </div>
-      <WithLoaderAndError {...{ data, isError, isLoading, error }}>
-        <div className="flex flex-wrap justify-between items-start gap-6">
-          <BasketProducts
-            item={data}
-            listCourse={data?.listCourse}
-            handleDeleteProduct={handleBasket}
-          />
-          <div className="flex flex-col bg-main-secondary-bg text-main-primary-text max-w-[25rem] w-full rounded-small">
-            <div className={cn(
-              "py-[1.4375rem] px-5",
-              textBody1Bold
-            )}>
-              <h1>جزئیات خرید</h1>
+    return (
+        <div className='flex flex-col gap-6'>
+            <div className='flex items-center rounded-small bg-main-secondary-bg border border-main-primary-text px-6 py-2.5 gap-2.5'>
+                <BasketIcon className='w-6 h-6 dark:invert' />
+                <h1 className={textTitle3}>سبد خرید</h1>
             </div>
-            <div className="flex border-t border-main-gray-50 px-2">
-              <div className="flex justify-center items-center rounded-small w-full">
-                <Input
-                  id="discountCode"
-                  value={discountCode}
-                  onChange={(e) => setDiscountCode(e.target.value)}
-                  placeHolder="کد تخفیف را اینجا وارد کنید و تیک را بزنید."
-                  intent="primary"
-                  className={cn(
-                    textBody3,
-                    "py-[0.5rem] px-5 h-full flex justify-center rounded-r-small border border-opacity-70 border-main-secondary-text"
-                  )}
-                />
-                <Button
-                  intent="primary"
-                  className="hover:shadow-none rounded-l-small border border-main-secondary-text border-r-0 border-opacity-70 py-[1.03125rem] px-4"
-                  onClick={handleCheckCode}
-                  disabled={discountLoading}
-                >
-                  <Tick fill="#1A1C21" />
-                </Button>
-              </div>
-            </div>
-            <BasketDetails
-              listCourse={data?.listCourse}
-              totalPrice={totalPrice}
-              totalDiscount={totalDiscount}
-              totalPriceWithDiscount={totalPriceWithDiscount}
-              discountResult={discountResult}
-            />
-            <div className="px-4 py-4 text-center border-t border-main-gray-50">
-              <Button
-                intent="primary"
-                size="fit"
-                className='py-4 px-[8.875rem] border article:px-0 article:w-full'
-                onClick={handlePurchase}
-              >
-                خرید دوره
-              </Button>
-            </div>
-          </div>
+            <WithLoaderAndError {...{ data, isError, isLoading, error }}>
+                <div className='flex flex-wrap justify-between items-start gap-6'>
+                    <BasketProducts listCourse={products} handleDeleteProduct={handleBasket} />
+                    <div className='flex flex-col bg-main-secondary-bg text-main-primary-text max-w-[25rem] w-full rounded-small'>
+                        <div className={cn('py-[1.4375rem] px-5', textBody1Bold)}>
+                            <h2>جزئیات خرید</h2>
+                        </div>
+                        <div className='flex border-t border-main-gray-50 px-2'>
+                            <div className='flex justify-center items-center rounded-small w-full'>
+                                <Input
+                                    id='discountCode'
+                                    value={discountCode}
+                                    onChange={e => setDiscountCode(e.target.value)}
+                                    placeHolder='کد تخفیف را اینجا وارد کنید.'
+                                    intent='primary'
+                                    className={cn(
+                                        textBody3,
+                                        'py-[0.5rem] px-5 h-full flex justify-center rounded-r-small border border-opacity-70 border-main-secondary-text'
+                                    )}
+                                />
+                                <Button
+                                    intent='primary'
+                                    className='hover:shadow-none rounded-l-small border border-main-secondary-text border-r-0 border-opacity-70 py-[1.03125rem] px-4'
+                                    onClick={handleCheckCode}
+                                    disabled={discountLoading}>
+                                    <Tick fill='#1A1C21' />
+                                </Button>
+                            </div>
+                        </div>
+                        <BasketDetails
+                            listCourse={products}
+                            totalPrice={totalPrice}
+                            totalDiscount={totalDiscount}
+                            totalPriceWithDiscount={totalPriceWithDiscount}
+                            discountResult={discountResult}
+                        />
+                        <div className='px-4 py-4 text-center border-t border-main-gray-50'>
+                            <Button
+                                intent='primary'
+                                size='fit'
+                                className='py-4 px-[8.875rem] border article:px-0 article:w-full'
+                                onClick={handlePurchase}>
+                                نکمیل خرید
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </WithLoaderAndError>
         </div>
-      </WithLoaderAndError>
-    </div>
-  );
+    );
 };
 
 export default Basket;
